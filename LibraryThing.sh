f276c4ba8cad0a699a74fc36f13ce965454fb6d5
Victor Engmark <victor.engmark@gmail.com>
2011-02-22 19:54:41 +0100

    Documentation formatting

--- LibraryThing.sh
+++ LibraryThing.sh
@@ -7,8 +7,8 @@
 #        LibraryThing.sh <username> <password> <save path>
 #
 # DESCRIPTION
-#        Downloads the entire library of a single user in comma-separated
-#        values (CSV) format.
+#        Downloads the entire library of a single user in comma-separated values
+#        (CSV) format.
 #
 #        How to save your personal library at midnight every day:
 #
@@ -26,23 +26,23 @@
 #        know.
 #
 # BUGS
-#    https://github.com/l0b0/export/issues
+#        https://github.com/l0b0/export/issues
 #
 # COPYRIGHT AND LICENSE
 #        Copyright (C) 2010, 2011 Victor Engmark
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#        GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #################################################################################
 set -o errexit -o nounset
