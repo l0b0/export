l0b0/export

commit bb7f0938acf42f12fdf77b69b8dfb69ab99e0f1a

Got rid of unnecessary HTTP request

WordPress.com.sh
@@ -61,7 +61,6 @@ EXPORT_PATH="$4"
 COOKIES_PATH="${EXPORT_PATH}.cookie"
 LOGIN_URL="https://${HOSTNAME}.wordpress.com/wp-login.php"
 
-curl --cookie-jar "$COOKIES_PATH" --output /dev/null "$LOGIN_URL" # TODO: Necessary??
 curl --cookie-jar "$COOKIES_PATH" --output /dev/null --data "log=${USERNAME}&pwd=${PASSWORD}&rememberme=forever&wp-submit=Log In&redirect_to=https://${HOSTNAME}.wordpress.com/wp-admin/&testcookie=1" "$LOGIN_URL"
 
 # Cookie cleansing

commit 5c39dc3faf20b3b6c6d73075edf37b6c427f179a

Fix cURL cookie handling
- Have to first request the login page (regression in bb7f0938acf42f12fdf77b69b8dfb69ab99e0f1a).
- Have to save the cookies in the first command, and load+save them in the second command.

WordPress.com.sh
@@ -68,7 +68,8 @@ then
     trap 'rm -f -- "$cookies_path"' EXIT
 fi
 
-curl --insecure --cookie-jar "$cookies_path" --output /dev/null \
+curl --insecure --cookie-jar "$cookies_path" --output /dev/null "$login_url"
+curl --insecure --cookie "$cookies_path" --cookie-jar "$cookies_path" --output /dev/null \
     --data-urlencode "log=${username}" \
     --data-urlencode "pwd=${password}" \
     --data-urlencode "rememberme=forever" \